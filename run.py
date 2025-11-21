979754dcf29fb15c7a99511863a1693635017472
Nicholas <nchlseaton3@gmail.com>
2025-11-21 13:33:42 -0500

    Drop all tables using db.drop_all()

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 from app import create_app
+from app.models import db
 
 app = create_app()
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
