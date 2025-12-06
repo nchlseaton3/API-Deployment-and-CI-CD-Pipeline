4aae04230a511aa64bc01e82cf40d0b754a5ec52
Nicholas <nchlseaton3@gmail.com>
2025-12-05 19:04:26 -0500

    Installed needed pips, Created swagger.yaml file, Plug swagger ui into flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
 from .extensions import db, ma, limiter, cache 
 from .mechanic import mechanic_bp
 from .service_ticket import service_ticket_bp
@@ -24,7 +25,17 @@ def create_app():
     app.register_blueprint(service_ticket_bp, url_prefix="/service-tickets")
     app.register_blueprint(parts_bp, url_prefix="/parts")
 
+    # Swagger UI
+    swagger_url = "/docs"
+    api_url = "/static/swagger.yaml"
 
+    swaggerui_bp = get_swaggerui_blueprint(
+        swagger_url,
+        api_url,
+        config={"app_name": "Mechanic Shop API"}
+    )
+    app.register_blueprint(swaggerui_bp, url_prefix=swagger_url)
+    
     # Auto-create tables
     with app.app_context():
         db.create_all()
