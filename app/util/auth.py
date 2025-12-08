49fc0cc4b34fdbe142ce134fe685b08dd5b8ede2
Nicholas <nchlseaton3@gmail.com>
2025-12-08 12:26:20 -0500

    Create PostgreSQL Database and hid sensitive information

--- app/util/auth.py
+++ app/util/auth.py
@@ -3,8 +3,9 @@ from jose import jwt
 import jose
 from functools import wraps
 from flask import request, jsonify
+import os
 
-SECRET_KEY = 'super secret secrets'
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'super secret secrets'
 
 def encode_token(user_id, role="mechanic"):
    
